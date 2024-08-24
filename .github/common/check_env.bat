f353c14511bc0fac2405757cdc1e5745d7e9a6c6
mjreno <mjreno@IGSAAA071L01144.gs.doi.net>
2024-08-24 18:49:05 -0400

    t2

--- .github/common/check_env.bat
+++ .github/common/check_env.bat
@@ -1,5 +1,9 @@
+pixi shell
+which meson
+which pkg-config
 pkg-config --list-all
 pkg-config --libs netcdf
 pkg-config --libs netcdf-fortran
 pkg-config --cflags netcdf
 pkg-config --cflags netcdf-fortran
+exit

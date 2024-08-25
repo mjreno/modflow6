5c1f79783ea969bdde04bac213350292534e2c48
mjreno <mjreno@IGSAAA071L01144.gs.doi.net>
2024-08-25 11:59:21 -0400

    t2

--- .github/common/compile_zlib.bat
+++ .github/common/compile_zlib.bat
@@ -1,1 +1,19 @@
 echo "COMPILE ZLIB"
+ls -l "$GITHUB_WORKSPACE"
+cd "$GITHUB_WORKSPACE/zlib"
+export CC=icc
+export CXX=icpc
+export F77=ifort
+export FC=ifort
+export F90=ifort
+export CPP='icc -E'
+export CXXCPP='icpc -E'
+#export CFLAGS='-O3 -xHost -ip -no-prec-div -static-intel'
+#export CXXFLAGS='-O3 -xHost -ip -no-prec-div -static-intel'
+#export FFLAGS='-O3 -xHost -ip -no-prec-div -static-intel'
+export CFLAGS='-O3 -xHost -ip -no-prec-div'
+export CXXFLAGS='-O3 -xHost -ip -no-prec-div'
+export FFLAGS='-O3 -xHost -ip -no-prec-div'
+./configure --prefix=$(pwd) --enable-shared
+make check
+make install

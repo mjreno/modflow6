265b206ad500aeb4e4bb632edb3904c57e5fa9c6
mjreno <mjreno@IGSAAA071L01144.gs.doi.net>
2024-08-25 09:49:04 -0400

    t2

--- .github/common/compile_zlib.sh
+++ .github/common/compile_zlib.sh
@@ -1,15 +1,16 @@
-cd "$GITHUB_WORKSPACE/zlib"
 echo "COMPILE ZLIB"
+ls -l "$GITHUB_WORKSPACE"
+cd "$GITHUB_WORKSPACE/zlib"
 export CC=icc
 export CXX=icpc
-#export CFLAGS='-O3 -xHost -ip -no-prec-div -static-intel'
-#export CXXFLAGS='-O3 -xHost -ip -no-prec-div -static-intel'
 export F77=ifort
 export FC=ifort
 export F90=ifort
-#export FFLAGS='-O3 -xHost -ip -no-prec-div -static-intel'
 export CPP='icc -E'
 export CXXCPP='icpc -E'
+#export CFLAGS='-O3 -xHost -ip -no-prec-div -static-intel'
+#export CXXFLAGS='-O3 -xHost -ip -no-prec-div -static-intel'
+#export FFLAGS='-O3 -xHost -ip -no-prec-div -static-intel'
 export CFLAGS='-O3 -xHost -ip -no-prec-div'
 export CXXFLAGS='-O3 -xHost -ip -no-prec-div'
 export FFLAGS='-O3 -xHost -ip -no-prec-div'

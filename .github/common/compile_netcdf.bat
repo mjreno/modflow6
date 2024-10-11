72d4798be6690d7bba2baa88b9a5e73a87aad8a7
mjreno <mjreno@IGSAAA071L01144.gs.doi.net>
2024-10-11 17:42:27 -0400

    t5

--- .github/common/compile_netcdf.bat
+++ .github/common/compile_netcdf.bat
@@ -31,7 +31,7 @@ cmake --build . --config Release
 cmake –install .
 
 :: BUILD/INSTALL SHARED LIBS
-cmake --fresh -G "Visual Studio 17 2022" -A x64 -DCMAKE_Fortran_COMPILER="C:/Program Files (x86)/Intel/oneAPI/compiler/latest/bin/ifort.exe" -DNETCDF_C_LIBRARY="%GITHUB_WORKSPACE%/netcdf/netCDF4.9.2-NC4-64/lib/netcdf.lib" -DNETCDF_C_INCLUDE_DIR="%GITHUB_WORKSPACE%/netcdf/netCDF4.9.2-NC4-64/include" -DBUILD_SHARED_LIBS=1 -DCMAKE_INSTALL_PREFIX="%GITHUB_WORKSPACE%/netcdf/netcdf-fortran-4.6.1_build/build" ../netcdf-fortran-4.6.1
+cmake --fresh -G Ninja -DCMAKE_Fortran_COMPILER="C:/Program Files (x86)/Intel/oneAPI/compiler/latest/bin/ifort.exe" -DNETCDF_C_LIBRARY="%GITHUB_WORKSPACE%/netcdf/netCDF4.9.2-NC4-64/lib/netcdf.lib" -DNETCDF_C_INCLUDE_DIR="%GITHUB_WORKSPACE%/netcdf/netCDF4.9.2-NC4-64/include" -DBUILD_SHARED_LIBS=1 -DCMAKE_INSTALL_PREFIX="%GITHUB_WORKSPACE%/netcdf/netcdf-fortran-4.6.1_build/build" ../netcdf-fortran-4.6.1
 cmake --build . --config Release
 cmake –install .
 
@@ -42,9 +42,9 @@ set PATH=%GITHUB_WORKSPACE%\netcdf\netcdf-fortran-4.6.1_build\build\fortran\Rele
 set PATH=%GITHUB_WORKSPACE%\netcdf\netCDF4.9.2-NC4-64\bin;%PATH%
 set PATH=%GITHUB_WORKSPACE%\netcdf\netcdf-fortran-4.6.1_build\build\bin;%PATH%
 
-:: set FC=ifort
-:: cd "%GITHUB_WORKSPACE%\modflow6"
-:: pixi run setup -Dnetcdf=true builddir
-:: pixi run build builddir
-:: pixi run test builddir
-:: pixi run pytest -v --durations=0 --keep-failed .failed --netcdf -k "test_netcdf"
+set FC=ifort
+cd "%GITHUB_WORKSPACE%\modflow6"
+pixi run setup -Dnetcdf=true builddir
+pixi run build builddir
+pixi run test builddir
+pixi run pytest -v --durations=0 --keep-failed .failed --netcdf -k "test_netcdf"

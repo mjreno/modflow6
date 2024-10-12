b631d61fbf5fab0a344d5fb037a927f5eeb2f46a
mjreno <mjreno@IGSAAA071L01144.gs.doi.net>
2024-10-12 13:03:11 -0400

    t6

--- .github/common/compile_netcdf.bat
+++ .github/common/compile_netcdf.bat
@@ -42,9 +42,10 @@ dir .
 dir fortran
 dir libsrc
 dir D:\a\modflow6\modflow6\modflow6\..\netcdf
-nf-config
+dir %GITHUB_WORKSPACE%\netcdf\netcdf-fortran-4.6.1_build\build\fortran
+.\nf-config --all
 
-set PKG_CONFIG_PATH=%GITHUB_WORKSPACE%\netcdf\netcdf-fortran-4.6.1_build\build\lib\pkgconfig\;%PKG_CONFIG_PATH%
+set PKG_CONFIG_PATH=%GITHUB_WORKSPACE%\netcdf\netcdf-fortran-4.6.1_build\build;%PKG_CONFIG_PATH%
 set PKG_CONFIG_PATH=%GITHUB_WORKSPACE%\netcdf\netCDF4.9.2-NC4-64\lib\pkgconfig\;%PKG_CONFIG_PATH%
 set PATH=%GITHUB_WORKSPACE%\netcdf\netCDF4.9.2-NC4-64\include;%PATH%
 set PATH=%GITHUB_WORKSPACE%\netcdf\netcdf-fortran-4.6.1_build\build\fortran;%PATH%

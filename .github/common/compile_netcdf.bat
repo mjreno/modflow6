48ef436ee6d7f0200b867573b97951c31ae535ee
mjreno <mjreno@IGSAAA071L01144.gs.doi.net>
2024-10-12 11:23:58 -0400

    t6

--- .github/common/compile_netcdf.bat
+++ .github/common/compile_netcdf.bat
@@ -30,6 +30,9 @@ cmake --fresh -G Ninja -DCMAKE_Fortran_COMPILER="C:/Program Files (x86)/Intel/on
 cmake --build . --config Release
 cmake –install . --config Release
 
+dir .
+dir fortran
+
 :: BUILD/INSTALL SHARED LIBS
 cmake --fresh -G Ninja -DCMAKE_Fortran_COMPILER="C:/Program Files (x86)/Intel/oneAPI/compiler/latest/bin/ifort.exe" -DNETCDF_C_LIBRARY="%GITHUB_WORKSPACE%/netcdf/netCDF4.9.2-NC4-64/lib/netcdf.lib" -DNETCDF_C_INCLUDE_DIR="%GITHUB_WORKSPACE%/netcdf/netCDF4.9.2-NC4-64/include" -DBUILD_SHARED_LIBS=1 -DCMAKE_INSTALL_PREFIX="%GITHUB_WORKSPACE%/netcdf/netcdf-fortran-4.6.1_build/build" ../netcdf-fortran-4.6.1
 cmake --build . --config Release

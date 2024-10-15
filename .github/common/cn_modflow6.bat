e777112a9650f996196b9469cccc724942ca5c6f
mjreno <mjreno@IGSAAA071L01144.gs.doi.net>
2024-10-15 13:48:28 -0400

    t7

--- .github/common/cn_modflow6.bat
+++ .github/common/cn_modflow6.bat
@@ -9,17 +9,17 @@ set PATH=%GITHUB_WORKSPACE%\netcdf\netcdf-fortran-4.6.1_build\build;%PATH%
 set LIBPATH=%GITHUB_WORKSPACE%\netcdf\netCDF4.9.2-NC4-64\lib;%LIBPATH%
 set LIBPATH=%GITHUB_WORKSPACE%\netcdf\netcdf-fortran-4.6.1_build\build\fortran;%LIBPATH%
 
-dir %GITHUB_WORKSPACE%\netcdf\netcdf-fortran-4.6.1_build\build
-dir %GITHUB_WORKSPACE%\netcdf\netcdf-fortran-4.6.1_build\build\fortran
+:: dir %GITHUB_WORKSPACE%\netcdf\netcdf-fortran-4.6.1_build\build
+:: dir %GITHUB_WORKSPACE%\netcdf\netcdf-fortran-4.6.1_build\build\fortran
 
-echo "%PKG_CONFIG_PATH%"
-echo "%PATH%"
+:: echo "%PKG_CONFIG_PATH%"
+:: echo "%PATH%"
 
-cd "%GITHUB_WORKSPACE%\netcdf"
-ifort /include:%GITHUB_WORKSPACE%\netcdf\netCDF4.9.2-NC4-64\include /include:%GITHUB_WORKSPACE%\netcdf\netcdf-fortran-4.6.1_build\build\fortran simple.f90 /link /libpath:%GITHUB_WORKSPACE%\netcdf\netcdf-fortran-4.6.1_build\build\fortran /libpath:%GITHUB_WORKSPACE%\netcdf\netCDF4.9.2-NC4-64\lib netcdff.lib netcdf.lib
+:: cd "%GITHUB_WORKSPACE%\netcdf"
+:: ifort /include:%GITHUB_WORKSPACE%\netcdf\netCDF4.9.2-NC4-64\include /include:%GITHUB_WORKSPACE%\netcdf\netcdf-fortran-4.6.1_build\build\fortran simple.f90 /link /libpath:%GITHUB_WORKSPACE%\netcdf\netcdf-fortran-4.6.1_build\build\fortran /libpath:%GITHUB_WORKSPACE%\netcdf\netCDF4.9.2-NC4-64\lib netcdff.lib netcdf.lib
 
-simple.exe
-dir .
+:: simple.exe
+:: dir .
 
 where nc-config
 where nf-config

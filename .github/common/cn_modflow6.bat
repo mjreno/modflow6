afe381b53e6332cfc220c39ff631d98d4844e5ba
mjreno <mjreno@IGSAAA071L01144.gs.doi.net>
2024-10-15 13:20:03 -0400

    t6

--- .github/common/cn_modflow6.bat
+++ .github/common/cn_modflow6.bat
@@ -30,4 +30,4 @@ set FC=ifort
 cd "%GITHUB_WORKSPACE%\modflow6"
 pixi run setup -Dnetcdf=true builddir
 pixi run build builddir
-pixi run pytest -v --durations=0 --netcdf -k "test_netcdf"
+:: pixi run pytest -v --durations=0 --netcdf -k "test_netcdf"

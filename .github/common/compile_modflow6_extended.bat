0ee679cfcd858dea67a7b2065e7ab8d0df760225
mjreno <mjreno@IGSAAA071L01144.gs.doi.net>
2024-10-16 19:06:30 -0400

    t1

--- .github/common/compile_modflow6_extended.bat
+++ .github/common/compile_modflow6_extended.bat
@@ -1,6 +1,9 @@
 set FC=ifort
 cd "%GITHUB_WORKSPACE%\modflow6"
 dir ..
+dir ..\netcdf
+dir ..\netcdf\netCDF4.9.2-NC4-64
+dir ..\netcdf\netCDF4.9.2-NC4-64\include
 dir ..\..
 pixi run setup -Dextended=true builddir
 pixi run build builddir

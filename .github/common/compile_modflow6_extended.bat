7f88fc53f016bc0b418087d405388bda3f82dac6
mjreno <mjreno@IGSAAA071L01144.gs.doi.net>
2024-10-17 13:34:28 -0400

    t2

--- .github/common/compile_modflow6_extended.bat
+++ .github/common/compile_modflow6_extended.bat
@@ -1,11 +1,7 @@
 set FC=ifort
 cd "%GITHUB_WORKSPACE%\modflow6"
-dir ..
-dir ..\netcdf
-dir ..\netcdf\netCDF4.9.2-NC4-64
 dir ..\netcdf\netCDF4.9.2-NC4-64\lib
 dir ..\netcdf\netCDF4.9.2-NC4-64\bin
-dir ..\netcdf\netcdf-fortran-4.6.1
 dir ..\netcdf\netcdf-fortran-4.6.1\build
 dir ..\netcdf\netcdf-fortran-4.6.1\build\fortran
 pixi run setup -Dextended=true builddir

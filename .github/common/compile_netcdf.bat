5531f8553c44e936d23a6e8ebace1253a63051d6
mjreno <mjreno@IGSAAA071L01144.gs.doi.net>
2024-10-31 17:34:34 -0400

    t0_3

--- .github/common/compile_netcdf.bat
+++ .github/common/compile_netcdf.bat
@@ -1,5 +1,9 @@
 cd "%GITHUB_WORKSPACE%\netcdf\netcdf-fortran-4.6.1\build"
 
+dir "%GITHUB_WORKSPACE%\netcdf\netCDF4.9.2-NC4-64\bin"
+dir "%GITHUB_WORKSPACE%\netcdf\netCDF4.9.2-NC4-64\lib"
+dir "%GITHUB_WORKSPACE%\netcdf\netCDF4.9.2-NC4-64\include"
+
 :: build/install static libs
 cmake --fresh -G Ninja -DCMAKE_Fortran_COMPILER="C:/Program Files (x86)/Intel/oneAPI/compiler/latest/bin/ifort.exe" -DNETCDF_C_LIBRARY="%GITHUB_WORKSPACE%/netcdf/netCDF4.9.2-NC4-64/lib/netcdf.lib" -DNETCDF_C_INCLUDE_DIR="%GITHUB_WORKSPACE%/netcdf/netCDF4.9.2-NC4-64/include" -DBUILD_SHARED_LIBS=0 -DCMAKE_INSTALL_PREFIX="%GITHUB_WORKSPACE%/netcdf/netcdf-fortran-4.6.1/build" ../netcdf-fortran-4.6.1
 cmake --build .
@@ -9,3 +13,5 @@ cmake –install .
 cmake --fresh -G Ninja -DCMAKE_Fortran_COMPILER="C:/Program Files (x86)/Intel/oneAPI/compiler/latest/bin/ifort.exe" -DNETCDF_C_LIBRARY="%GITHUB_WORKSPACE%/netcdf/netCDF4.9.2-NC4-64/lib/netcdf.lib" -DNETCDF_C_INCLUDE_DIR="%GITHUB_WORKSPACE%/netcdf/netCDF4.9.2-NC4-64/include" -DBUILD_SHARED_LIBS=1 -DCMAKE_INSTALL_PREFIX="%GITHUB_WORKSPACE%/netcdf/netcdf-fortran-4.6.1/build" ../netcdf-fortran-4.6.1
 cmake --build .
 cmake –install .
+
+dir "%GITHUB_WORKSPACE%\netcdf\netcdf-fortran-4.6.1\build\fortran"

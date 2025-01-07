77441dc90962c067995f152afe0cc13eb4abb940
mjreno <mjreno@IGSAAA071L01144.gs.doi.net>
2025-01-07 14:54:11 -0500

    t2

--- .github/common/test_modflow6_extended.bat
+++ .github/common/test_modflow6_extended.bat
@@ -1,2 +1,4 @@
 cd "%GITHUB_WORKSPACE%\modflow6\autotest"
-pixi run autotest -m "%MARKERS%" --netcdf --parallel -s
+pytest -v test_gwf_sto01.py -s
+pytest -v test_netcdf_gwf_sto01.py --netcdf -s
+pixi run autotest -m "%MARKERS%" --netcdf --parallel

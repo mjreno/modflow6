66dea15fccc65df22ef62940bbd4b0af3aa4c2d0
mjreno <mjreno@IGSAAA071L01144.gs.doi.net>
2025-02-25 16:09:44 -0500

    update GWT MST and IST for Idm

--- src/Utilities/Idm/mf6blockfile/LoadMf6File.f90
+++ src/Utilities/Idm/mf6blockfile/LoadMf6File.f90
@@ -376,12 +376,17 @@ contains
     end if
 
     if (.not. found) then
-      ! load standard keyword tag
-      call load_keyword_type(this%parser, idt, this%mf6_input%mempath, this%iout)
-      ! check/set as dev option
-      if (idt%tagname(1:4) == 'DEV_' .and. &
-          this%mf6_input%block_dfns(iblk)%blockname == 'OPTIONS') then
-        call this%parser%DevOpt()
+      if (idt%shape == ':') then
+        call load_string_type(this%parser, idt, this%mf6_input%mempath, this%iout)
+      else
+        ! load standard keyword tag
+        call load_keyword_type(this%parser, idt, this%mf6_input%mempath, &
+                               this%iout)
+        ! check/set as dev option
+        if (idt%tagname(1:4) == 'DEV_' .and. &
+            this%mf6_input%block_dfns(iblk)%blockname == 'OPTIONS') then
+          call this%parser%DevOpt()
+        end if
       end if
     end if
   end subroutine parse_keyword_tag
@@ -400,6 +405,7 @@ contains
     logical(LGP), intent(in) :: recursive_call !< true if recursive call
     character(len=LINELENGTH) :: tag
     type(InputParamDefinitionType), pointer :: idt !< input data type object describing this record
+    integer(I4B) :: itag
 
     ! read tag name
     call this%parser%GetStringCaps(tag)
@@ -421,6 +427,7 @@ contains
     select case (idt%datatype)
     case ('KEYWORD')
       call this%parse_keyword_tag(iblk, tag, idt)
+      if (idt%shape == ':') return
     case ('STRING')
       if (idt%shape == 'NAUX') then
         call load_auxvar_names(this%parser, idt, this%mf6_input%mempath, &
@@ -459,14 +466,15 @@ contains
       call this%parser%StoreErrorUnit()
     end select
 
+    call expandarray(this%block_tags)
+    itag = size(this%block_tags)
+    this%block_tags(itag) = trim(idt%tagname)
+
     ! continue line if in same record
     if (idt%in_record) then
       ! recursively call parse tag again to read rest of line
       call this%parse_tag(iblk, .true.)
     end if
-
-    call expandarray(this%block_tags)
-    this%block_tags(size(this%block_tags)) = trim(idt%tagname)
   end subroutine parse_tag
 
   function block_index_dfn(this, iblk) result(idt)
@@ -621,8 +629,16 @@ contains
     integer(I4B), intent(in) :: iout !< unit number for output
     character(len=LINELENGTH), pointer :: cstr
     character(len=LENBIGLINE), pointer :: bigcstr
+    character(len=:), allocatable :: line
     integer(I4B) :: ilen
     select case (idt%shape)
+    case (':')
+      ilen = LENBIGLINE
+      call parser%GetRemainingLine(line)
+      call mem_allocate(bigcstr, ilen, idt%mf6varname, memoryPath)
+      bigcstr = line
+      call idm_log_var(bigcstr, idt%tagname, memoryPath, iout)
+      deallocate (line)
     case ('LENBIGLINE')
       ilen = LENBIGLINE
       call mem_allocate(bigcstr, ilen, idt%mf6varname, memoryPath)

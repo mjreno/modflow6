9bb8ddce3c5329953aca52ca2887acb7fbb2608e
mjreno <mjreno@IGSAAA071L01144.gs.doi.net>
2025-07-31 09:18:15 -0400

    regenerate idm files

---
+++ src/Idm/gwe-eslidm.f90
@@ -0,0 +1,437 @@
+! ** Do Not Modify! MODFLOW 6 system generated file. **
+module GweEslInputModule
+  use ConstantsModule, only: LENVARNAME
+  use InputDefinitionModule, only: InputParamDefinitionType, &
+                                   InputBlockDefinitionType
+  private
+  public gwe_esl_param_definitions
+  public gwe_esl_aggregate_definitions
+  public gwe_esl_block_definitions
+  public GweEslParamFoundType
+  public gwe_esl_multi_package
+  public gwe_esl_subpackages
+
+  type GweEslParamFoundType
+    logical :: auxiliary = .false.
+    logical :: auxmultname = .false.
+    logical :: boundnames = .false.
+    logical :: print_input = .false.
+    logical :: print_flows = .false.
+    logical :: save_flows = .false.
+    logical :: ts_filerecord = .false.
+    logical :: ts6 = .false.
+    logical :: filein = .false.
+    logical :: ts6_filename = .false.
+    logical :: obs_filerecord = .false.
+    logical :: obs6 = .false.
+    logical :: obs6_filename = .false.
+    logical :: maxbound = .false.
+    logical :: cellid = .false.
+    logical :: senerrate = .false.
+    logical :: aux = .false.
+    logical :: boundname = .false.
+  end type GweEslParamFoundType
+
+  logical :: gwe_esl_multi_package = .true.
+
+  character(len=16), parameter :: &
+    gwe_esl_subpackages(*) = &
+    [ &
+    '                ' &
+    ]
+
+  type(InputParamDefinitionType), parameter :: &
+    gweesl_auxiliary = InputParamDefinitionType &
+    ( &
+    'GWE', & ! component
+    'ESL', & ! subcomponent
+    'OPTIONS', & ! block
+    'AUXILIARY', & ! tag name
+    'AUXILIARY', & ! fortran variable
+    'STRING', & ! type
+    'NAUX', & ! shape
+    'keyword to specify aux variables', & ! longname
+    .false., & ! required
+    .false., & ! multi-record
+    .false., & ! preserve case
+    .false., & ! layered
+    .false. & ! timeseries
+    )
+
+  type(InputParamDefinitionType), parameter :: &
+    gweesl_auxmultname = InputParamDefinitionType &
+    ( &
+    'GWE', & ! component
+    'ESL', & ! subcomponent
+    'OPTIONS', & ! block
+    'AUXMULTNAME', & ! tag name
+    'AUXMULTNAME', & ! fortran variable
+    'STRING', & ! type
+    '', & ! shape
+    'name of auxiliary variable for multiplier', & ! longname
+    .false., & ! required
+    .false., & ! multi-record
+    .false., & ! preserve case
+    .false., & ! layered
+    .false. & ! timeseries
+    )
+
+  type(InputParamDefinitionType), parameter :: &
+    gweesl_boundnames = InputParamDefinitionType &
+    ( &
+    'GWE', & ! component
+    'ESL', & ! subcomponent
+    'OPTIONS', & ! block
+    'BOUNDNAMES', & ! tag name
+    'BOUNDNAMES', & ! fortran variable
+    'KEYWORD', & ! type
+    '', & ! shape
+    '', & ! longname
+    .false., & ! required
+    .false., & ! multi-record
+    .false., & ! preserve case
+    .false., & ! layered
+    .false. & ! timeseries
+    )
+
+  type(InputParamDefinitionType), parameter :: &
+    gweesl_print_input = InputParamDefinitionType &
+    ( &
+    'GWE', & ! component
+    'ESL', & ! subcomponent
+    'OPTIONS', & ! block
+    'PRINT_INPUT', & ! tag name
+    'PRINT_INPUT', & ! fortran variable
+    'KEYWORD', & ! type
+    '', & ! shape
+    'print input to listing file', & ! longname
+    .false., & ! required
+    .false., & ! multi-record
+    .false., & ! preserve case
+    .false., & ! layered
+    .false. & ! timeseries
+    )
+
+  type(InputParamDefinitionType), parameter :: &
+    gweesl_print_flows = InputParamDefinitionType &
+    ( &
+    'GWE', & ! component
+    'ESL', & ! subcomponent
+    'OPTIONS', & ! block
+    'PRINT_FLOWS', & ! tag name
+    'PRINT_FLOWS', & ! fortran variable
+    'KEYWORD', & ! type
+    '', & ! shape
+    'print calculated flows to listing file', & ! longname
+    .false., & ! required
+    .false., & ! multi-record
+    .false., & ! preserve case
+    .false., & ! layered
+    .false. & ! timeseries
+    )
+
+  type(InputParamDefinitionType), parameter :: &
+    gweesl_save_flows = InputParamDefinitionType &
+    ( &
+    'GWE', & ! component
+    'ESL', & ! subcomponent
+    'OPTIONS', & ! block
+    'SAVE_FLOWS', & ! tag name
+    'SAVE_FLOWS', & ! fortran variable
+    'KEYWORD', & ! type
+    '', & ! shape
+    'save well flows to budget file', & ! longname
+    .false., & ! required
+    .false., & ! multi-record
+    .false., & ! preserve case
+    .false., & ! layered
+    .false. & ! timeseries
+    )
+
+  type(InputParamDefinitionType), parameter :: &
+    gweesl_ts_filerecord = InputParamDefinitionType &
+    ( &
+    'GWE', & ! component
+    'ESL', & ! subcomponent
+    'OPTIONS', & ! block
+    'TS_FILERECORD', & ! tag name
+    'TS_FILERECORD', & ! fortran variable
+    'RECORD TS6 FILEIN TS6_FILENAME', & ! type
+    '', & ! shape
+    '', & ! longname
+    .false., & ! required
+    .false., & ! multi-record
+    .false., & ! preserve case
+    .false., & ! layered
+    .false. & ! timeseries
+    )
+
+  type(InputParamDefinitionType), parameter :: &
+    gweesl_ts6 = InputParamDefinitionType &
+    ( &
+    'GWE', & ! component
+    'ESL', & ! subcomponent
+    'OPTIONS', & ! block
+    'TS6', & ! tag name
+    'TS6', & ! fortran variable
+    'KEYWORD', & ! type
+    '', & ! shape
+    'head keyword', & ! longname
+    .true., & ! required
+    .true., & ! multi-record
+    .false., & ! preserve case
+    .false., & ! layered
+    .false. & ! timeseries
+    )
+
+  type(InputParamDefinitionType), parameter :: &
+    gweesl_filein = InputParamDefinitionType &
+    ( &
+    'GWE', & ! component
+    'ESL', & ! subcomponent
+    'OPTIONS', & ! block
+    'FILEIN', & ! tag name
+    'FILEIN', & ! fortran variable
+    'KEYWORD', & ! type
+    '', & ! shape
+    'file keyword', & ! longname
+    .true., & ! required
+    .true., & ! multi-record
+    .false., & ! preserve case
+    .false., & ! layered
+    .false. & ! timeseries
+    )
+
+  type(InputParamDefinitionType), parameter :: &
+    gweesl_ts6_filename = InputParamDefinitionType &
+    ( &
+    'GWE', & ! component
+    'ESL', & ! subcomponent
+    'OPTIONS', & ! block
+    'TS6_FILENAME', & ! tag name
+    'TS6_FILENAME', & ! fortran variable
+    'STRING', & ! type
+    '', & ! shape
+    'file name of time series information', & ! longname
+    .true., & ! required
+    .true., & ! multi-record
+    .true., & ! preserve case
+    .false., & ! layered
+    .false. & ! timeseries
+    )
+
+  type(InputParamDefinitionType), parameter :: &
+    gweesl_obs_filerecord = InputParamDefinitionType &
+    ( &
+    'GWE', & ! component
+    'ESL', & ! subcomponent
+    'OPTIONS', & ! block
+    'OBS_FILERECORD', & ! tag name
+    'OBS_FILERECORD', & ! fortran variable
+    'RECORD OBS6 FILEIN OBS6_FILENAME', & ! type
+    '', & ! shape
+    '', & ! longname
+    .false., & ! required
+    .false., & ! multi-record
+    .false., & ! preserve case
+    .false., & ! layered
+    .false. & ! timeseries
+    )
+
+  type(InputParamDefinitionType), parameter :: &
+    gweesl_obs6 = InputParamDefinitionType &
+    ( &
+    'GWE', & ! component
+    'ESL', & ! subcomponent
+    'OPTIONS', & ! block
+    'OBS6', & ! tag name
+    'OBS6', & ! fortran variable
+    'KEYWORD', & ! type
+    '', & ! shape
+    'obs keyword', & ! longname
+    .true., & ! required
+    .true., & ! multi-record
+    .false., & ! preserve case
+    .false., & ! layered
+    .false. & ! timeseries
+    )
+
+  type(InputParamDefinitionType), parameter :: &
+    gweesl_obs6_filename = InputParamDefinitionType &
+    ( &
+    'GWE', & ! component
+    'ESL', & ! subcomponent
+    'OPTIONS', & ! block
+    'OBS6_FILENAME', & ! tag name
+    'OBS6_FILENAME', & ! fortran variable
+    'STRING', & ! type
+    '', & ! shape
+    'obs6 input filename', & ! longname
+    .true., & ! required
+    .true., & ! multi-record
+    .true., & ! preserve case
+    .false., & ! layered
+    .false. & ! timeseries
+    )
+
+  type(InputParamDefinitionType), parameter :: &
+    gweesl_maxbound = InputParamDefinitionType &
+    ( &
+    'GWE', & ! component
+    'ESL', & ! subcomponent
+    'DIMENSIONS', & ! block
+    'MAXBOUND', & ! tag name
+    'MAXBOUND', & ! fortran variable
+    'INTEGER', & ! type
+    '', & ! shape
+    'maximum number of sources', & ! longname
+    .true., & ! required
+    .false., & ! multi-record
+    .false., & ! preserve case
+    .false., & ! layered
+    .false. & ! timeseries
+    )
+
+  type(InputParamDefinitionType), parameter :: &
+    gweesl_cellid = InputParamDefinitionType &
+    ( &
+    'GWE', & ! component
+    'ESL', & ! subcomponent
+    'PERIOD', & ! block
+    'CELLID', & ! tag name
+    'CELLID', & ! fortran variable
+    'INTEGER1D', & ! type
+    'NCELLDIM', & ! shape
+    'cell identifier', & ! longname
+    .true., & ! required
+    .true., & ! multi-record
+    .false., & ! preserve case
+    .false., & ! layered
+    .false. & ! timeseries
+    )
+
+  type(InputParamDefinitionType), parameter :: &
+    gweesl_senerrate = InputParamDefinitionType &
+    ( &
+    'GWE', & ! component
+    'ESL', & ! subcomponent
+    'PERIOD', & ! block
+    'SENERRATE', & ! tag name
+    'SENERRATE', & ! fortran variable
+    'DOUBLE', & ! type
+    '', & ! shape
+    'energy source loading rate', & ! longname
+    .true., & ! required
+    .true., & ! multi-record
+    .false., & ! preserve case
+    .false., & ! layered
+    .true. & ! timeseries
+    )
+
+  type(InputParamDefinitionType), parameter :: &
+    gweesl_aux = InputParamDefinitionType &
+    ( &
+    'GWE', & ! component
+    'ESL', & ! subcomponent
+    'PERIOD', & ! block
+    'AUX', & ! tag name
+    'AUX', & ! fortran variable
+    'DOUBLE1D', & ! type
+    'NAUX', & ! shape
+    'auxiliary variables', & ! longname
+    .false., & ! required
+    .true., & ! multi-record
+    .false., & ! preserve case
+    .false., & ! layered
+    .true. & ! timeseries
+    )
+
+  type(InputParamDefinitionType), parameter :: &
+    gweesl_boundname = InputParamDefinitionType &
+    ( &
+    'GWE', & ! component
+    'ESL', & ! subcomponent
+    'PERIOD', & ! block
+    'BOUNDNAME', & ! tag name
+    'BOUNDNAME', & ! fortran variable
+    'STRING', & ! type
+    '', & ! shape
+    'well name', & ! longname
+    .false., & ! required
+    .true., & ! multi-record
+    .false., & ! preserve case
+    .false., & ! layered
+    .false. & ! timeseries
+    )
+
+  type(InputParamDefinitionType), parameter :: &
+    gwe_esl_param_definitions(*) = &
+    [ &
+    gweesl_auxiliary, &
+    gweesl_auxmultname, &
+    gweesl_boundnames, &
+    gweesl_print_input, &
+    gweesl_print_flows, &
+    gweesl_save_flows, &
+    gweesl_ts_filerecord, &
+    gweesl_ts6, &
+    gweesl_filein, &
+    gweesl_ts6_filename, &
+    gweesl_obs_filerecord, &
+    gweesl_obs6, &
+    gweesl_obs6_filename, &
+    gweesl_maxbound, &
+    gweesl_cellid, &
+    gweesl_senerrate, &
+    gweesl_aux, &
+    gweesl_boundname &
+    ]
+
+  type(InputParamDefinitionType), parameter :: &
+    gweesl_spd = InputParamDefinitionType &
+    ( &
+    'GWE', & ! component
+    'ESL', & ! subcomponent
+    'PERIOD', & ! block
+    'STRESS_PERIOD_DATA', & ! tag name
+    'SPD', & ! fortran variable
+    'RECARRAY CELLID SENERRATE AUX BOUNDNAME', & ! type
+    'MAXBOUND', & ! shape
+    '', & ! longname
+    .true., & ! required
+    .false., & ! multi-record
+    .false., & ! preserve case
+    .false., & ! layered
+    .false. & ! timeseries
+    )
+
+  type(InputParamDefinitionType), parameter :: &
+    gwe_esl_aggregate_definitions(*) = &
+    [ &
+    gweesl_spd &
+    ]
+
+  type(InputBlockDefinitionType), parameter :: &
+    gwe_esl_block_definitions(*) = &
+    [ &
+    InputBlockDefinitionType( &
+    'OPTIONS', & ! blockname
+    .false., & ! required
+    .false., & ! aggregate
+    .false. & ! block_variable
+    ), &
+    InputBlockDefinitionType( &
+    'DIMENSIONS', & ! blockname
+    .true., & ! required
+    .false., & ! aggregate
+    .false. & ! block_variable
+    ), &
+    InputBlockDefinitionType( &
+    'PERIOD', & ! blockname
+    .true., & ! required
+    .true., & ! aggregate
+    .true. & ! block_variable
+    ) &
+    ]
+
+end module GweEslInputModule

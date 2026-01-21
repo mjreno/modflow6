6cf997a8e487b151eee4f550d3f96d431f53efdb
mjreno <mjreno@IGSAAA071L01144.gs.doi.net>
2026-01-21 17:03:52 -0500

    expose crs in docs

--- src/Idm/swf-dis2didm.f90
+++ src/Idm/swf-dis2didm.f90
@@ -241,7 +241,7 @@ module SwfDis2DInputModule
     'LENBIGLINE', & ! shape
     'CRS user input string', & ! longname
     .false., & ! required
-    .true., & ! developmode
+    .false., & ! developmode
     .false., & ! multi-record
     .true., & ! preserve case
     .false., & ! layered

b004b7bce6eac4627eafe6baedd06fd612748d3b
mjreno <renomik@gmail.com>
2026-03-09 15:02:12 -0400

    add path override and diagnostic

--- .github/common/test_modflow6_extended.bat
+++ .github/common/test_modflow6_extended.bat
@@ -1,2 +1,3 @@
+set PATH=%GITHUB_WORKSPACE%\netcdf\netCDF4.9.3-NC4-64\bin;%GITHUB_WORKSPACE%\netcdf\netcdf-fortran-4.6.2\build\fortran;%PATH%
 cd "%GITHUB_WORKSPACE%\modflow6\autotest"
 pixi run autotest -m "%MARKERS%" -k "%FILTERS%" --netcdf --parallel

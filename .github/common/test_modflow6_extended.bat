6e61664dc332add2538c9a82439df7599f093b4c
mjreno <renomik@gmail.com>
2026-03-09 15:51:04 -0400

    build against conda-forge netcdf-c

--- .github/common/test_modflow6_extended.bat
+++ .github/common/test_modflow6_extended.bat
@@ -1,3 +1,2 @@
-set PATH=%GITHUB_WORKSPACE%\netcdf\netCDF4.9.3-NC4-64\bin;%GITHUB_WORKSPACE%\netcdf\netcdf-fortran-4.6.2\build\fortran;%PATH%
 cd "%GITHUB_WORKSPACE%\modflow6\autotest"
 pixi run autotest -m "%MARKERS%" -k "%FILTERS%" --netcdf --parallel
